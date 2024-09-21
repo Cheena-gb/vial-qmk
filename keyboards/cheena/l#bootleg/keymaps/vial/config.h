fce6aaee29e1eb2eac8058a145fb99b415e520f4
Sierra <rinn.kawakami@gmail.com>
2024-09-22 03:20:05 +0900

    add L#Bootleg(wip)

---
+++ keyboards/cheena/l#bootleg/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x40, 0x11, 0x64, 0xD8, 0x68, 0xD0, 0x1A, 0x73}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0,1}
+#define VIAL_UNLOCK_COMBO_COLS {1,0}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
