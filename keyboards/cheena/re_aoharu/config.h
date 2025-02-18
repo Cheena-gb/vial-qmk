5e3ecb3a40b9502dfdf4960a5058e9b6939c18ae
Sierra <rinn.kawakami@gmail.com>
2025-02-19 03:53:44 +0900

    RE Aoharu LED matrix update

--- keyboards/cheena/re_aoharu/config.h
+++ keyboards/cheena/re_aoharu/config.h
@@ -13,6 +13,8 @@
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
+#define RGB_MATRIX_CENTER { 105, 32 }
+
 #ifndef IOS_DEVICE_ENABLE
   #define RGBLIGHT_LIMIT_VAL 180
   #define RGBLIGHT_VAL_STEP 17
