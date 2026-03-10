fd3271edc0674685eaf8047ca3cbebda3292cc1e
Sierra <rinn.kawakami@gmail.com>
2026-03-11 03:21:18 +0900

    add tabpad50

---
+++ keyboards/cheena/tabpad/config.h
@@ -0,0 +1,35 @@
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 5
+
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#undef LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#undef LOCKING_RESYNC_ENABLE
+
+#define RGB_TRIGGER_ON_KEYDOWN
+
+#ifndef IOS_DEVICE_ENABLE
+  #define RGBLIGHT_LIMIT_VAL 180
+  #define RGBLIGHT_VAL_STEP 17
+#else
+  #define RGBLIGHT_LIMIT_VAL 50
+  #define RGBLIGHT_VAL_STEP 4
+#endif
+
+#if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
+// USB_MAX_POWER_CONSUMPTION value for naked48 keyboard
+//  120  RGBoff, OLEDoff
+//  120  OLED
+//  330  RGB 6
+//  300  RGB 32
+//  310  OLED & RGB 32
+  #define USB_MAX_POWER_CONSUMPTION 400
+#else
+  // fix iPhone and iPad power adapter issue
+  // iOS device need lessthan 100
+  #define USB_MAX_POWER_CONSUMPTION 100
+#endif
---
+++ keyboards/cheena/tabpad/rules.mk
@@ -0,0 +1,12 @@
+# Build Options
+# change yes to no to disable
+#
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+
+WS2812_DRIVER = vendor
+RGB_MATRIX_DRIVER = ws2812
+
+CUSTOM_MATRIX = lite
+SRC += matrix.c
